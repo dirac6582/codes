dfc02d2ee03ef28856d6c1b744fd3eaef204d665
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2021-10-28 18:59:04 +0900

    add diary

---
+++ mount_sauron.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##
+#
+# 2021/10/28 ver1
+# mount ssh server to local
+#
+##
+
+## ----------------------------
+# do shell script "/usr/local/bin/sshfs ユーザ名@ホスト名:接続先のフォルダのパス マウントポイント用のパス -oIdentityFile=~/.ssh/id_rsa -p ポート番号"
+#
+# --ユーザ名：自分がそのサーバーにログインする時のユーザー名
+# --ホスト名：接続したいホスト名
+# --接続先のフォルダのパス：サーバーでの自分のホームフォルダにするといいと思います
+# --マウントポイントのパス：自分が作ったマウントさせたいフォルダへのパス
+# --ポート番号：プロバイダによっては接続してほしいポート番号が違っていたりするみたいです。プロバイダのマニュアル参照の事
+## ----------------------------
+
+# sauronへの接続
+#sshfs amano@sauron3:/home/amano ~/sauron -oIdentityFile ~/.ssh/id_sauron_imacnew -p 22
+
+sshfs amano@sauron3:/home/amano ~/sauron -p 22
+
