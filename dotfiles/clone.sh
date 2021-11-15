99aef9119f7de39461cf0e2b259fdfb0bb694c73
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2021-11-16 02:07:21 +0900

    modify clone.sh

--- dotfiles/clone.sh
+++ dotfiles/clone.sh
@@ -4,7 +4,7 @@
 # cloning other repositories
 
 # z.sh
-git clone git://github.com/rupa/z ${HOME}/src/
+git clone git://github.com/rupa/z ${HOME}/src/z/
 
 
 
@@ -14,8 +14,8 @@ git clone git://github.com/rupa/z ${HOME}/src/
 #Menlo-for-Powerline/ → homebrewへ変更
 # nerd-fonts/ → homebrewへ変更
 
-git clone git@github.com:seebi/dircolors-solarized.git ${PWD}/iterm2setting
-git clone git@github.com:altercation/solarized.git ${PWD}/iterm2setting
+git clone git@github.com:seebi/dircolors-solarized.git ${PWD}/iterm2setting/dircolors-solarized/
+git clone git@github.com:altercation/solarized.git ${PWD}/iterm2setting/solarized/
 # git clone git@github.com:abertsch/Menlo-for-Powerline.git ${PWD}/iterm2setting
 # git clone git@github.com:ryanoasis/nerd-fonts.git ${PWD}/iterm2setting
 
